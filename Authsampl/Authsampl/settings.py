b7c51df071a2f46a0e0069bca698e097e5473e44
umarulshahin <akkushahin666@gmail.com>
2024-05-28 02:29:18 +0530

    bug fix

--- Authsampl/Authsampl/settings.py
+++ Authsampl/Authsampl/settings.py
@@ -60,9 +60,9 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
 
     "ALGORITHM": "HS256",
